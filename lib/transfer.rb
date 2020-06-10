d3809f30e1761f088a35e40c6292a009241248ec
Samantha Wabi <swfullpotential@gmail.com>
2020-06-10 21:11:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,12 +15,12 @@ class Transfer
   end
   
   def execute_transaction
-    if sender.balance > amount && status == "pending" && valid?
-      sender.balance -= amount
-      receiver.balance += amount
-      status = "complete"
+    if @sender.balance > @amount && @status == "pending" && valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
     else
-      status = "rejected"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
